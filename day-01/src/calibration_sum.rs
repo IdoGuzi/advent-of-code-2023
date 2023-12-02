de4ae7e29b06f6abc9566ce4e78fecbc30833875
Ido Guzi <idoguzi@hotmail.com>
2023-12-02 15:23:42 +0200

    part 2

--- day-01/src/calibration_sum.rs
+++ day-01/src/calibration_sum.rs
@@ -1,33 +1,47 @@
 use std::fs;
 use std::error::Error;
+use std::collections::HashMap;
 
-fn find_digit(text: &mut Vec<char>, reverse: bool) -> Result<usize, Box<dyn Error>> {
-    if reverse {
-        text.reverse();
-    }
-    for i in 0..text.len() {
-        match text[i] {
-            '0' | '1' |'2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                if !reverse {
-                    return Ok(i);
+static NUMBERS: &[&str] = &[
+    "1", "one",
+    "2", "two",
+    "3", "three",
+    "4", "four",
+    "5", "five",
+    "6", "six",
+    "7", "seven",
+    "8", "eight",
+    "9", "nine"
+];
+
+fn find_digit(text: &str, reverse: bool) -> Result<u8, Box<dyn Error>> {
+    let mut indices: HashMap<u8, u8> = HashMap::new();
+    for (i,v) in NUMBERS.iter().enumerate() {
+        let index_option; 
+        if !reverse {index_option = text.find(v);}else{index_option = text.rfind(v);}
+        match index_option {
+            None => continue,
+            Some(index) => {
+                if !indices.contains_key(&(index as u8)) {
+                    indices.insert(index as u8, ((i/2) + 1) as u8);
                 }
-                return Ok(text.len()-1 - i);
-            },
-            _ => continue,
+            }
         };
     }
-    let original_string: String = text.iter().collect();
-    let err = ["digit not found in ", &original_string].join("");
-    return Err(err.into());
+    if indices.is_empty() {
+        return Err(["no digits were found in: ", text].join("").into())
+    }
+    if reverse {
+        return Ok(*indices.get(indices.keys().max().unwrap()).unwrap());
+    }
+    return Ok(*indices.get(indices.keys().min().unwrap()).unwrap());
 }
 
 fn extract_calibration_value(text: &str) -> Result<u8, Box<dyn Error>> {
-    let vec: Vec<char> = text.chars().collect();
-    let first = find_digit(&mut vec.clone(), false)?;
-    let last = find_digit(&mut vec.clone(), true)?;
-    let value: u8 = (vec[first] as u8 - 48) * 10 + (vec[last] as u8 - 48);
+    let first = find_digit(text, false)?;
+    let last = find_digit(text, true)?;
+    let value: u8 = first * 10 + last;
     return Ok(value);
-
 }
 
 pub fn read_file(inputs_path: String) -> Result<u64, Box<dyn Error>> {
